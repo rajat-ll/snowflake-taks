411eacb0dcd831ed7ebb2995375a6ca3cd326c65
rajat-ll <153066047+rajat-ll@users.noreply.github.com>
2024-11-28 09:59:38 +0530

    Create irr_task.sql

---
+++ irr_task.sql
@@ -0,0 +1,22 @@
+create or replace task LL_PROD_RAW_ZONE.TEMP.NDV_MIFOS_IRR_POS_DATA_REFRESH_TASK
+	warehouse=LL_STG_WH
+	schedule='USING CRON 0 10 1 * * Asia/Kolkata'
+	as BEGIN
+    CREATE OR REPLACE TEMPORARY TABLE NDV_MIFOS_IRR_POS_DATA_OPENING_TEST AS
+    SELECT ml.id, ml.expected_xirr,
+        SUM(IFF(
+                ZEROIFNULL(mlirs.principal_amount) >
+                (ZEROIFNULL(mlirs.principal_completed_derived) +
+                ZEROIFNULL(mlirs.principal_writtenoff_derived)), 1, 0)
+        ) AS pending_emi_count,
+        ROUND(SUM(
+                ZEROIFNULL(mlirs.principal_amount) -
+                (ZEROIFNULL(mlirs.principal_completed_derived) +
+                ZEROIFNULL(mlirs.principal_writtenoff_derived))), 2
+        ) AS irr_pos
+    FROM m_loan ml
+    LEFT OUTER JOIN m_loan_irr_repayment_schedule mlirs
+    ON ml.id = mlirs.loan_id
+    WHERE ml.loan_status_id IN (300, 600, 700)
+    GROUP BY ALL;
+END;
